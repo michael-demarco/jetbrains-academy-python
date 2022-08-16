4ea71aa7596722225522d2a9aaab79de6f7169a4
Michael DeMarco <michael.demarco@uconn.edu>
2022-08-15 23:44:10 -0400

    Chat Bot

--- Chatty-Bot/main.py
+++ Chatty-Bot/main.py
@@ -1,25 +1,58 @@
-print('Hello! My name is Aid.')
-print('I was created in 2022.')
-print('Please, remind me your name.')
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
 
-name = input()
 
-print('What a great name you have, ' + name + '!')
-print('Let me guess your age.')
-print('Enter remainders of dividing your age by 3, 5 and 7.')
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
 
-rem3 = int(input())
-rem5 = int(input())
-rem7 = int(input())
 
-age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-print("Your age is " + str(age) + "; that's a good time to start programming!")
-print('Now I will prove to you that I can count to any number you want.')
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-# read a number and count to it here
-count = abs(int(input()))
-for i in range(0, count + 1) :
-    print(i, "!")
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
 
-print('Completed, have a nice day!')
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    # write your code here
+    switch = {
+        1: '1. To repeat a statement multiple times.',
+        2: '2. To decompose a program into several small subroutines.',
+        3: '3. To determine the execution time of a program.',
+        4: '4. To interrupt the execution of a program.'
+    }
+    num = input()
+    return switch.get(num, "Invalid input")
+
+    print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
+end()
